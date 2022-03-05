baac3d99477cf141a6ca394269eda6095ebffed4
zirei <zeatgomez@gmail.com>
2022-03-04 19:02:57 -0500

    proyecto finalizado

---
+++ workspace/bank-balance/src/test/java/com/vobi/bank/BankBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vobi.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankBalanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
